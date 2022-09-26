d799aaf8b985e707ed3c6f745494b49759bc5fda
naurunnahansa <114341308+naurunnahansa@users.noreply.github.com>
2022-09-26 16:08:22 +0530

    Update op01.py

--- my_dagster_project/assets/op01.py
+++ my_dagster_project/assets/op01.py
@@ -12,13 +12,20 @@ def my_opp():
 
 @op
 def myApple():
-    return "Apple"
+    print("asdas")
+
+@op
+def get_file_sizes():
+    files = [f for f in os.listdir(".") if os.path.isfile(f)]
+    for f in files:
+        get_dagster_logger().info(f"Size of {f} is {os.path.getsize(f)}")
 
 @job
 def file_sizes_job():
-    myApple()
+    get_file_sizes()
 
 if __name__ == "__main__":
     print("Runnnn!")
     materialize([my_op,my_opp])
     result = file_sizes_job.execute_in_process()
+    print(result)
